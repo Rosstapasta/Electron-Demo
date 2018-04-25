b2cfbbfd27a9259d661e421194f0846d8b35f102
SethLee <sethlimlee@gmail.com>
2018-04-25 11:43:00 -0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src='https://media.licdn.com/dms/image/C5603AQEa_T0nPeYfpQ/profile-displayphoto-shrink_800_800/0?e=1529863200&v=beta&t=0XDO3Dev_lVtASBuREwzpIZY2gbuURQdAlECHjJGH5E' className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
