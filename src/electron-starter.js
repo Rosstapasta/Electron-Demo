2c6ee84c8c93e88989a5d914e7b8dbdff82c91f4
SethLee <sethlimlee@gmail.com>
2018-04-25 12:25:34 -0600

    Final Commit...for now ;)

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,11 +1,6 @@
 const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const {app, BrowserWindow} = electron
 
-const path = require('path')
-const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -13,7 +8,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:3000')
